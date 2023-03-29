afce7a7b5d835b08bafb39e8f3849af3b2e9f763
mcmcmax437 <mcmcmax437@gmail.com>
2023-03-29 14:17:01 +0300

    Basic functional - only most important method

--- MiniMax_MulthiThread_vs_Basic/Program.cs
+++ MiniMax_MulthiThread_vs_Basic/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics; 
+using System.Xml.Linq;
+
+namespace MiniMax
+{
+    class Program
+    {
+            // A simple class to represent a node in the game tree
+            class Node
+            {
+               
+            }
+
+            // A method to generate a random game tree with a given depth and branching factor
+            static Node GenerateTree(int depth, int branchingFactor)
+            {
+              
+            }
+
+            // A method to print a game tree in a readable format
+            static void PrintTree(Node node, int level)
+            {
+               
+            }
+
+        
+            static int MiniMax(Node node, bool isMaximizing)
+            {
+               
+            }
+
+           
+            static async Task<int> MiniMaxAsync(Node node, bool isMaximizing)
+            {
+             
+            }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
